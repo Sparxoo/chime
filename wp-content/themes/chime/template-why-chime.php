dd84c2942e7c4b4686ea90308b2bc3f91537e7c9
pm@pixelcrayons.com <pm@pixelcrayons.com>
2015-05-22 16:03:48 +0530

    Done

--- wp-content/themes/chime/template-why-chime.php
+++ wp-content/themes/chime/template-why-chime.php
@@ -9,24 +9,38 @@
 	//add the hero banner to the header.
 	get_template_part("content", "hero"); 
 ?>
-<div class="service-detail pull-left">
+<div class="after-banner why_chime_div pull-left">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4 col-sm-4 col-xs-12 pull-right">
-				<div class="testimonial-image">
-					<img src="<?php the_field('client_image', $post->ID);?>" alt="">
-				</div>
+			<div class="col-md-12 col-sm-12 col-xs-12 text-center ">
+				<h1 class="sub_heading_chime"><?php the_field('sub_heading');?></h1>
 			</div>
-			<div class="col-md-7 col-sm-7 col-xs-12 pull-left">
-				<div class="testimonial-text">
-					<h3><?php the_field('testimonial_title', $post->ID);?></h3>
-					<p><?php the_field('testimonial', $post->ID);?> <img src="<?php bloginfo('template_url');?>/images/testimonial-end.png" alt=""></p>
-					<span><?php the_field('client_name', $post->ID);?></span>
-				</div>				
+			<div class="col-md-2 col-sm-2 col-xs-12"></div>
+			<div class="col-md-8 col-sm-8 col-xs-12 text-center">
+				<p><?php the_field('sub_description');?></p>
+			</div>
+			<div class="col-md-12 col-sm-12 col-xs-12">
+<?php if( have_rows('what_we_value') ){
+	while ( have_rows('what_we_value') ) : the_row();
+	?>
+	<div class="col-md-12 col-sm-12 col-xs-12 gap_div">
+	<div class="col-md-3 col-sm-3 col-xs-12 green_bg_div">
+		<?php the_sub_field('title', $post->ID); ?>
+	</div>
+	<div class="col-md-9 col-sm-9 col-xs-12 blue_bg_div">
+		<?php the_sub_field('discription', $post->ID);?>
+	</div>
+</div>
+<?php endwhile; }?>
 			</div>
 		</div>
 	</div>
-</div>	
+</div>
+
+
+
+
+
 
 <div class="top-3-reasons pull-left" style="background-image:url(<?php the_field('reasons_background', $post->ID);?>);background-size:cover;background-position:center left;">
 	<div class="container">
@@ -46,7 +60,27 @@
 		</div>
 	</div>
 </div>
+<!--<div class="service-detail pull-left">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-4 col-xs-12 pull-right">
+				<div class="testimonial-image">
+					<img src="<?php the_field('client_image', $post->ID);?>" alt="">
+				</div>
+			</div>
+			<div class="col-md-7 col-sm-7 col-xs-12 pull-left">
+				<div class="testimonial-text">
+					<h3><?php the_field('testimonial_title', $post->ID);?></h3>
+					<p><?php the_field('testimonial', $post->ID);?> <img src="<?php bloginfo('template_url');?>/images/testimonial-end.png" alt=""></p>
+					<span><?php the_field('client_name', $post->ID);?></span>
+
+				</div>				
+			</div>
+		</div>
+	</div>
+</div>	
+
 <?php 
 	get_template_part("content", "services"); 
-?>
+?>-->
 <?php get_footer(); ?>
